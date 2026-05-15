cec6acf6e6c3e57e4e6db64f73834d556d7e5f1b
Fardean <fardeanaymagh22@gmail.com>
2026-05-15 03:08:38 +0100

    Improve loading state across screens

---
+++ lib/models/app_user.dart
@@ -0,0 +1,11 @@
+class AppUser {
+  const AppUser({
+    required this.uid,
+    required this.email,
+    required this.name,
+  });
+
+  final String uid;
+  final String email;
+  final String name;
+}
